690099757ef719f132e12030515b4d80d9d3cc16
olympia15 <olympiamichelle15@gmail.com>
2026-04-12 20:26:06 -0400

    employee-details component implemented

---
+++ src/app/components/employee-details/employee-details.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+import { EmployeeService, Employee } from '../../services/employee.service';
+
+// Component to display details of employee
+@Component({
+  selector: 'app-employee-details',
+  imports: [CommonModule, RouterLink],
+  standalone: true,
+  templateUrl: './employee-details.html',
+  styleUrl: './employee-details.css',
+})
+
+export class EmployeeDetails implements OnInit {
+  employee: Employee | null = null;
+  loading = false;
+  errorMsg = '';
+
+  // Inject EmployeeService and ActivatedRoute to fetch employee details based on route parameter
+  constructor(private employeeService: EmployeeService, private route: ActivatedRoute) {}
+
+  // Fetch employee details on component initialization
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') || '';
+    this.loading = true;
+    this.employeeService.getEmployeeById(id).subscribe({
+      next: (emp) => { this.employee = emp; this.loading = false; },
+      error: (err) => { this.errorMsg = err.message; this.loading = false; },
+    });
+  }
+}
