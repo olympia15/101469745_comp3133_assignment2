3fd8d58ec42280d3f9108aa34e2757f8e5627c0d
olympia15 <olympiamichelle15@gmail.com>
2026-04-12 21:35:12 -0400

    added global styling to style.css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideApollo } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { inject } from '@angular/core';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+      return {
+        link: httpLink.create({ uri: environment.graphqlUri }),
+        cache: new InMemoryCache(),
+        defaultOptions: {
+          watchQuery: { fetchPolicy: 'network-only' },
+        },
+      };
+    }),
+  ],
 };
